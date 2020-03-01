055bdb24b83e386311435c5c7a511e6a4c85e4da
Mohamed Jawadi <mohamedjawedi93@gmail.com>
2020-03-01 11:07:00 +0100

    Update LoginActivity.java

--- app/src/main/java/com/md/zeroXCTF/LoginActivity.java
+++ app/src/main/java/com/md/zeroXCTF/LoginActivity.java
@@ -14,6 +14,7 @@ import com.mikepenz.iconics.context.IconicsLayoutInflater2;
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
     Button btnL;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         LayoutInflaterCompat.setFactory2(getLayoutInflater(), new IconicsLayoutInflater2(getDelegate()));
@@ -25,12 +26,42 @@ public class LoginActivity extends AppCompatActivity {
 
     private void init() {
         btnL=findViewById(R.id.btn_login);
+        mAuth= FirebaseAuth.getInstance();
+        exit=findViewById(R.id.ann);
+        ins=findViewById(R.id.inscri);
+        ecouteur();
     }
 
     public void signup(View view) {
         startActivity(new Intent(this, SignupActivity.class));
     }
 
+ =
+    private void ecouteur(){
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.getText().toString().IsEmpty)
+                    Toast.makeText(RegForm.this, "error user", Toast.LENGTH_SHORT).show();
+                if (email.getText().toString().IsEmpty)
+                    Toast.makeText(RegForm.this, "error email", Toast.LENGTH_SHORT).show();
+                if (pasword.getText().toString().IsEmpty)
+                    Toast.makeText(RegForm.this, "error pasword", Toast.LENGTH_SHORT).show();
+                else
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                        }
 
+                    });
+            }
+        });
+    }
 
 }
